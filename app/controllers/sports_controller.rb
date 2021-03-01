7d99858788e2d91a76a56625d2d0a781cdf895db
ryusyo621 <76475977+ryusyo621@users.noreply.github.com>
2021-03-01 17:23:13 +0900

    HTML調整

--- app/controllers/sports_controller.rb
+++ app/controllers/sports_controller.rb
@@ -1,6 +1,9 @@
 class SportsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
+  before_action :search_sport, only: [:index, :search, :show]
+
   def index
+    @sports = Sport.all
     @sport = Sport.order("created_at DESC")
   end
 
@@ -44,9 +47,17 @@ class SportsController < ApplicationController
     end
   end
 
+  def search
+    @results = @p.result.includes(:category)
+  end
+
   private
 
   def sport_params
     params.require(:sport).permit(:title, :sport_text, :category_id, :area_id, :price, :image).merge(user_id: current_user.id)
+  end
+
+  def search_sport
+    @p = Sport.ransack(params[:q])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "sports#index"
+  get 'sports/search'
   resources :sports do
     resources :messages, only: :create
   end
