e26a249bd3052c451eb203299a9f683ed00fc719
ryusyo621 <76475977+ryusyo621@users.noreply.github.com>
2021-03-02 21:25:58 +0900

    新規登録修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,21 +7,20 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
-  # def edit
-  # end
+  def edit
+  end
 
-  # def update
-  #   if current_user.update(user_params)
-  #     redirect_to root_path
-  #   else
-  #     render :edit
-  #   end
-  # end
+  def update
+    if current_user.update(user_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
 
   private
 
   def user_params
-    params.require(:user).permit(:nickname, :email, :password, :last_name, :first_name, :kana_last_name, :kana_first_name,
-                                 :phone_number, :main_sport, :birthday)
+    params.require(:user).permit(:nickname, :email, :password, :last_name, :first_name, :kana_last_name, :kana_first_name,:phone_number, :main_sport, :birthday)
   end
 end
